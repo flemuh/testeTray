3f7a3faa9d463f73b6731f24ed74a25b0196cb42
Fernando Humel <flemuh@gmail.com>
2026-03-26 15:38:39 -0300

    Implementação de qualidade de codigo

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Casts\Attribute;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'google_id',
         'google_access_token',
@@ -29,31 +31,27 @@ class User extends Model
     protected function cpf(): Attribute
     {
         return Attribute::make(
-            set: fn (?string $value) => $value
-                ? preg_replace('/\D/', '', $value)
-                : null,
+            set: fn (?string $value) => $value ? preg_replace('/\D/', '', $value) : null
         );
     }
 
     public function isRegistrationComplete(): bool
     {
-        return !empty($this->name)
-            && !empty($this->cpf)
-            && !empty($this->birth_date);
+        return ! empty($this->name)
+            && ! empty($this->cpf)
+            && ! empty($this->birth_date);
     }
 
     protected function cpfFormatted(): Attribute
     {
         return Attribute::make(
-            get: fn ($value, $attributes) =>
-                isset($attributes['cpf'])
-                    ? preg_replace(
-                        "/(\d{3})(\d{3})(\d{3})(\d{2})/",
-                        "$1.$2.$3-$4",
-                        $attributes['cpf']
-                    )
-                    : null
+            get: fn ($attributes) => isset($attributes['cpf'])
+                ? preg_replace(
+                    '/(\d{3})(\d{3})(\d{3})(\d{2})/',
+                    '$1.$2.$3-$4',
+                    $attributes['cpf']
+                )
+                : null
         );
     }
-
 }
